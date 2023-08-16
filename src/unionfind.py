67478b546ec0e500583cf835b22e0e109e35f7f2
mono-0812 <frisk02.jar@gmail.com>
2023-08-17 00:26:52 +0900

    first commit

---
+++ src/unionfind.py
@@ -0,0 +1,27 @@
+class unionfind():
+    def __init__(self,n):
+        self.n=n
+        self.parents=[-1 for i in range(self.n)]
+
+    def find(self,x):
+        if self.parents[x]<0:
+            return x
+        self.parents[x]=self.find(self.parents[x])
+        return self.parents[x]
+    
+    def union(self,x,y):
+        x=self.find(x)
+        y=self.find(y)
+        if self.size(x)<self.size(y):
+            x,y=y,x
+        self.parents[x]+=self.parents[y]
+        self.parents[y]=x
+    
+    def size(self,x):
+        return -self.parents[self.find(x)]
+    
+    def is_same(self,x,y):
+        return int(self.find(x)==self.find(y))
+
+
+
---
+++ unionfind.test.py
@@ -0,0 +1,12 @@
+# verification-helper: PROBLEM https://judge.yosupo.jp/problem/unionfind
+
+from src.unionfind import unionfind
+
+N,Q=map(int,input().split())
+uf=unionfind(N)
+for i in range(Q):
+    t,u,v=map(int,input().split())
+    if t==0:
+        uf.union(u,v)
+    if t==1:
+        print(uf.is_same(u,v))
