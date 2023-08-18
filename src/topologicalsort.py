60eeedc67b3d78c3e6ed12007a46c8b369e0e568
mono-0812 <frisk02.jar@gmail.com>
2023-08-18 15:17:47 +0900

    top

--- src/topologicalsort.py
+++ src/topologicalsort.py
@@ -7,14 +7,13 @@ def topologicalsort(path):
         for j in path[i]:
             cnt[j]+=1
     q=collections.deque([i for i in range(n) if cnt[i]==0])
-    used=[0 for i in range(n)]
     res=[]
     while q:
         v=q.popleft()
         res.append(v)
         for i in path[v]:
-            if used[i]:continue
             cnt[i]-=1
             if not cnt[i]:
                 q.append(i)
-    return res
+
+    return res if sum(cnt)==0 else []
